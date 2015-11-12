81cb90dfd74fd35eb798033946f1ed9c8b85b4ba
Besat Zardosht <besat@binnj.com>
2015-11-12 11:05:02 -0500

    XEP-0313 Archiving Preferences

--- Extensions/XEP-0313/XMPPMessageArchiveManagement.h
+++ Extensions/XEP-0313/XMPPMessageArchiveManagement.h
@@ -6,9 +6,10 @@
 //  Copyright © 2015 binnj inc. All rights reserved.
 //
 
-#import "XMPPModule.h"
-#import "XMPPMessageArchivingManagementObject.h"
+#import "XMPP.h"
 #import "XMPPIDTracker.h"
+
+@protocol XMPPMessageArchivingStorage;
 
 /**
  * This class provides support for query and control an archive of messages stored on the server.
@@ -16,13 +17,84 @@
  **/
 @interface XMPPMessageArchiveManagement : XMPPModule
 {
-    XMPPIDTracker *responseTracker;
+    @protected
+        __strong id <XMPPMessageArchivingStorage> xmppMessageArchivingStorage;
+    @private
+        XMPPIDTracker *responseTracker;
+        NSXMLElement *preferences;
 }
 
-@property (strong,nonatomic) NSArray* results;
-@property (nonatomic) BOOL isResultCompleted;
+- (id)initWithMessageArchivingStorage:(id <XMPPMessageArchivingStorage>)storage;
+- (id)initWithMessageArchivingStorage:(id <XMPPMessageArchivingStorage>)storage dispatchQueue:(dispatch_queue_t)queue;
 
-- (void) fetchArchivedMessagesWithBareJid: (NSString*)withBareJid startTime:(NSDate*)startTime endTime:(NSDate*)endTime maxResultNumber: (NSInteger*)maxResultNumber;
-//- (void) fetchNextResultPage;
+@property (readonly, strong) id <XMPPMessageArchivingStorage> xmppMessageArchivingStorage;
+@property (readwrite, copy) NSXMLElement *preferences;
+
+- (void) syncLocalMessageArchiveWithServerMessageArchive;
+- (void) syncLocalMessageArchiveWithServerMessageArchiveWithBareJid: (NSString*)withBareJid startTime:(NSDate*)startTime endTime:(NSDate*)endTime maxResultNumber: (NSInteger*)maxResultNumber;
+
+@end
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark -
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+@protocol XMPPMessageArchivingStorage <NSObject>
+@required
+
+//
+//
+// -- PUBLIC METHODS --
+//
+// There are no public methods required by this protocol.
+//
+// Each individual roster storage class will provide a proper way to access/enumerate the
+// users/resources according to the underlying storage mechanism.
+//
+
+
+//
+//
+// -- PRIVATE METHODS --
+//
+// These methods are designed to be used ONLY by the XMPPMessageArchiving class.
+//
+//
+
+/**
+ * Configures the storage class, passing its parent and parent's dispatch queue.
+ *
+ * This method is called by the init method of the XMPPMessageArchiveManagement class.
+ * This method is designed to inform the storage class of its parent
+ * and of the dispatch queue the parent will be operating on.
+ *
+ * The storage class may choose to operate on the same queue as its parent,
+ * or it may operate on its own internal dispatch queue.
+ *
+ * This method should return YES if it was configured properly.
+ * If a storage class is designed to be used with a single parent at a time, this method may return NO.
+ * The XMPPMessageArchiving class is configured to ignore the passed
+ * storage class in its init method if this method returns NO.
+ **/
+- (BOOL)configureWithParent:(XMPPMessageArchiveManagement *)aParent queue:(dispatch_queue_t)queue;
+
+/**
+ *
+ **/
+- (void)archiveMessage:(XMPPMessage *)message outgoing:(BOOL)isOutgoing xmppStream:(XMPPStream *)stream;
+
+
+@optional
+
+/**
+ * The storage class may optionally persistently store the client preferences.
+ **/
+- (void)setPreferences:(NSXMLElement *)prefs forUser:(XMPPJID *)bareUserJid;
+
+/**
+ * The storage class may optionally persistently store the client preferences.
+ * This method is then used to fetch previously known preferences when the client first connects to the xmpp server.
+ **/
+- (NSXMLElement *)preferencesForUser:(XMPPJID *)bareUserJid;
 
 @end
--- Extensions/XEP-0313/XMPPMessageArchiveManagement.m
+++ Extensions/XEP-0313/XMPPMessageArchiveManagement.m
@@ -16,11 +16,211 @@ static const int xmppLogLevel = XMPP_LOG_LEVEL_WARN; // | XMPP_LOG_FLAG_TRACE;
 static const int xmppLogLevel = XMPP_LOG_LEVEL_WARN;
 #endif
 
-
-@interface XMPPMessageArchiveManagement (private)
-@end
+#define XMLNS_XMPP_ARCHIVE @"urn:xmpp:mam:1"
 
 @implementation XMPPMessageArchiveManagement
+
+- (id)init
+{
+    // This will cause a crash - it's designed to.
+    // Only the init methods listed in XMPPMessageArchiving.h are supported.
+    
+    return [self initWithMessageArchivingStorage:nil dispatchQueue:NULL];
+}
+
+- (id)initWithDispatchQueue:(dispatch_queue_t)queue
+{
+    // This will cause a crash - it's designed to.
+    // Only the init methods listed in XMPPMessageArchiving.h are supported.
+    
+    return [self initWithMessageArchivingStorage:nil dispatchQueue:queue];
+}
+
+- (id)initWithMessageArchivingStorage:(id <XMPPMessageArchivingStorage>)storage
+{
+    return [self initWithMessageArchivingStorage:storage dispatchQueue:NULL];
+}
+
+- (id)initWithMessageArchivingStorage:(id <XMPPMessageArchivingStorage>)storage dispatchQueue:(dispatch_queue_t)queue
+{
+    NSParameterAssert(storage != nil);
+    
+    if ((self = [super initWithDispatchQueue:queue]))
+    {
+        if ([storage configureWithParent:self queue:moduleQueue])
+        {
+            xmppMessageArchivingStorage = storage;
+        }
+        else
+        {
+            XMPPLogError(@"%@: %@ - Unable to configure storage!", THIS_FILE, THIS_METHOD);
+        }
+        
+//          <prefs xmlns='urn:xmpp:mam:1' default='always'>
+//          </prefs>
+        
+        NSXMLElement *pref = [NSXMLElement elementWithName:@"pref" xmlns:XMLNS_XMPP_ARCHIVE];
+        [pref addAttributeWithName:@"default" stringValue:@"always"];
+        
+        preferences = pref;
+    }
+    return self;
+}
+
+- (BOOL)activate:(XMPPStream *)aXmppStream
+{
+    XMPPLogTrace();
+    
+    if ([super activate:aXmppStream])
+    {
+        XMPPLogVerbose(@"%@: Activated", THIS_FILE);
+        
+        // Reserved for future potential use
+        
+        return YES;
+    }
+    
+    return NO;
+}
+
+- (void)deactivate
+{
+    XMPPLogTrace();
+    
+    // Reserved for future potential use
+    
+    [super deactivate];
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark Properties
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+- (id <XMPPMessageArchivingStorage>)xmppMessageArchivingStorage
+{
+    // Note: The xmppMessageArchivingStorage variable is read-only (set in the init method)
+    
+    return xmppMessageArchivingStorage;
+}
+
+- (NSXMLElement *)preferences
+{
+    __block NSXMLElement *result = nil;
+    
+    dispatch_block_t block = ^{
+        
+        result = [preferences copy];
+    };
+    
+    if (dispatch_get_specific(moduleQueueTag))
+        block();
+    else
+        dispatch_sync(moduleQueue, block);
+    
+    return result;
+}
+
+- (void)setPreferences:(NSXMLElement *)newPreferences
+{
+    dispatch_block_t block = ^{ @autoreleasepool {
+        
+        // Update cached value
+        
+        preferences = [newPreferences copy];
+        
+        // Update storage
+        
+        if ([xmppMessageArchivingStorage respondsToSelector:@selector(setPreferences:forUser:)])
+        {
+            XMPPJID *myBareJid = [[xmppStream myJID] bareJID];
+            
+            [xmppMessageArchivingStorage setPreferences:preferences forUser:myBareJid];
+        }
+        
+        //  - Send new pref to server
+        XMPPIQ *iq = [XMPPIQ iqWithType:@"set" to:nil elementID:nil child:preferences];
+        [xmppStream sendElement:iq];
+        
+    }};
+    
+    if (dispatch_get_specific(moduleQueueTag))
+        block();
+    else
+        dispatch_async(moduleQueue, block);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark XMPPStream Delegate
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+- (void)xmppStreamDidAuthenticate:(XMPPStream *)sender
+{
+    XMPPLogTrace();
+    
+    // Fetch most recent preferences
+    
+    if ([xmppMessageArchivingStorage respondsToSelector:@selector(preferencesForUser:)])
+    {
+        XMPPJID *myBareJid = [[xmppStream myJID] bareJID];
+        
+        preferences = [xmppMessageArchivingStorage preferencesForUser:myBareJid];
+    }
+    
+    // Request archiving preferences from server
+    //
+    // <iq type='get' id='juliet2'>
+    //   <prefs xmlns='urn:xmpp:mam:1'/>
+    // </iq>
+    
+    NSXMLElement *pref = [NSXMLElement elementWithName:@"prefs" xmlns:XMLNS_XMPP_ARCHIVE];
+    XMPPIQ *iq = [XMPPIQ iqWithType:@"get" to:nil elementID:nil child:pref];
+    
+    [sender sendElement:iq];
+}
+
+- (BOOL)xmppStream:(XMPPStream *)sender didReceiveIQ:(XMPPIQ *)iq
+{
+    NSString *type = [iq type];
+    
+    if ([type isEqualToString:@"result"])
+    {
+        NSXMLElement *pref = [iq elementForName:@"prefs" xmlns:XMLNS_XMPP_ARCHIVE];
+        if (pref)
+        {
+            [self setPreferences:pref];
+        }
+    }
+    return NO;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark Utilities
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+- (void) syncLocalMessageArchiveWithServerMessageArchive
+{
+    dispatch_block_t block = ^{ @autoreleasepool {
+        [self syncLocalMessageArchiveWithServerMessageArchiveWithBareJid:nil startTime:nil endTime:nil maxResultNumber:nil];
+    }};
+    
+    if (dispatch_get_specific(moduleQueueTag))
+        block();
+    else
+        dispatch_async(moduleQueue, block);
+}
+
+- (void) syncLocalMessageArchiveWithServerMessageArchiveWithBareJid: (NSString*)withBareJid startTime:(NSDate*)startTime endTime:(NSDate*)endTime maxResultNumber: (NSInteger*)maxResultNumber
+{
+    dispatch_block_t block = ^{ @autoreleasepool {
+        [self fetchArchivedMessagesWithBareJid:withBareJid startTime:startTime endTime:endTime maxResultNumber:maxResultNumber];
+    }};
+    
+    if (dispatch_get_specific(moduleQueueTag))
+        block();
+    else
+        dispatch_async(moduleQueue, block);
+    
+}
 
 - (void) fetchArchivedMessagesWithBareJid: (NSString*)withBareJid startTime:(NSDate*)startTime endTime:(NSDate*)endTime maxResultNumber: (NSInteger*)maxResultNumber
 {
@@ -50,9 +250,12 @@ static const int xmppLogLevel = XMPP_LOG_LEVEL_WARN;
 //          </query>
 //        </iq>
         
-        NSString* startTimeStr = [startTime xmppDateTimeString];
-        NSString* endTimeStr = [endTime xmppDateTimeString];
-        NSString* maxResultNumberStr = [NSString stringWithFormat:@"%ld",(long)maxResultNumber];
+        NSString* startTimeStr = @"";
+        NSString* endTimeStr = @"";
+        NSString* maxResultNumberStr = @"";
+        if (startTime) startTimeStr = [startTime xmppDateTimeString];
+        if (endTimeStr)  endTimeStr = [endTime xmppDateTimeString];
+        if (maxResultNumberStr)  maxResultNumberStr = [NSString stringWithFormat:@"%ld",(long)maxResultNumber];
         
         NSString *fetchID = [xmppStream generateUUID];
         
@@ -109,7 +312,7 @@ static const int xmppLogLevel = XMPP_LOG_LEVEL_WARN;
         [responseTracker addID:fetchID
                         target:self
                       selector:@selector(handleFetchArchivedMessageResponse:withInfo:)
-                       timeout:60.0];
+                       timeout:600.0];
         
     }};
     
