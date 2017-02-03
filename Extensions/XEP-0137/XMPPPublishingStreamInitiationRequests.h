c4554974444b2c0e96ff57227f6a7d82b67f97ef
Besat Zardosht <besat@morgiij.com>
2017-02-03 13:59:28 -0500

    Add XEP-0137 files

---
+++ Extensions/XEP-0137/XMPPPublishingStreamInitiationRequests.h
@@ -0,0 +1,13 @@
+//
+//  XMPPPublishingStreamInitiationRequests.h
+//  Mortgage
+//
+//  Created by besat@morgiij.com on 2017-02-03.
+//  Copyright © 2017 Morgiij Inc. All rights reserved.
+//
+
+#ifndef XMPPPublishingStreamInitiationRequests_h
+#define XMPPPublishingStreamInitiationRequests_h
+
+
+#endif /* XMPPPublishingStreamInitiationRequests_h */
---
+++ Extensions/XEP-0137/XMPPPublishingStreamInitiationRequests.m
@@ -0,0 +1,9 @@
+//
+//  XMPPPublishingStreamInitiationRequests.m
+//  Mortgage
+//
+//  Created by besat@morgiij.com on 2017-02-03.
+//  Copyright © 2017 Morgiij Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
