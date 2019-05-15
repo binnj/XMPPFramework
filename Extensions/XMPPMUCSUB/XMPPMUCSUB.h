50914875a7ccf6bdaf17a660b9f2f76d34ef5ee9
Besat Zardosht <besat@binnj.com>
2019-05-15 14:18:29 -0400

    Add new files

---
+++ Extensions/XMPPMUCSUB/XMPPMUCSUB.h
@@ -0,0 +1,13 @@
+//
+//  XMPPMUCSUB.h
+//  Dollarama
+//
+//  Created by Besat Zardosht on 2019-05-15.
+//  Copyright © 2019 binnj. All rights reserved.
+//
+
+#ifndef XMPPMUCSUB_h
+#define XMPPMUCSUB_h
+
+
+#endif /* XMPPMUCSUB_h */
---
+++ Extensions/XMPPMUCSUB/XMPPMUCSUB.m
@@ -0,0 +1,9 @@
+//
+//  XMPPMUCSUB.m
+//  Dollarama
+//
+//  Created by Besat Zardosht on 2019-05-15.
+//  Copyright © 2019 binnj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
